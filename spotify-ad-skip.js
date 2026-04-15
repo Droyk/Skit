0ac120b8533f58b59e711c3734d6151e0eba390c
Rumi <Droyk@users.noreply.github.com>
2026-04-15 19:28:22 +0530

    spotify-ad-skip.js

--- spotify-ad-skip.js
+++ spotify-ad-skip.js
@@ -1,26 +1,13 @@
-(function () {
+(function() {
     'use strict';
-    // If you see this, the script is LOADED but NOT interfering with network calls.
-    console.log('--- [SUCCESS] Spotify DOM-Skip Logic Active ---');
-
-    const AD_SELECTORS = [
-        '[data-testid="ad-label"]',
-        '[aria-label*="Advertisement" i]',
-        '[data-testid="advertisement"]',
-        '.encore-advertising-set'
-    ];
-
-    const observer = new MutationObserver(() => {
-        const hasAd = AD_SELECTORS.some(sel => document.querySelector(sel));
-        if (hasAd) {
-            const skipBtn = document.querySelector('[data-testid="control-button-skip-forward"]');
-            if (skipBtn && !skipBtn.disabled) {
-                console.log('Ad detected. Skipping...');
-                skipBtn.click();
-            }
+    console.log('--- LOG: SCRIPT START ---');
+    const skipAd = () => {
+        const btn = document.querySelector('[data-testid="control-button-skip-forward"]');
+        if (btn && !btn.disabled && document.querySelector('[data-testid="ad-label"], .encore-advertising-set')) {
+            console.log('--- LOG: AD SKIP CLICKED ---');
+            btn.click();
         }
-    });
-
-    // Start observing immediately
-    observer.observe(document.documentElement, { childList: true, subtree: true });
+    };
+    setInterval(skipAd, 1000);
+    console.log('--- LOG: SCRIPT FULLY LOADED ---');
 })();
