e87ed8595ee74da70538b88c57320e5592be0423
Rumi <Droyk@users.noreply.github.com>
2026-04-15 21:26:45 +0530

    spotify-ad-skip.js

--- spotify-ad-skip.js
+++ spotify-ad-skip.js
@@ -1,41 +1,46 @@
+/// spotify-ad-skip.js
+/// alias sas.js
 (function () {
     'use strict';
-    // The "Pulse" - if you see this in F12, the script survived line 1
-    console.log('--- [SPOTIFY-AD-SKIP] Script Injected & Running ---');
+    
+    // 1. Create a "Secret Flag" we can check in the console
+    window.__SKIT_ACTIVE__ = true; 
+    console.log('--- [SPOTIFY-AD-SKIP] Global Flag Set: window.__SKIT_ACTIVE__ ---');
 
     var _fetch = window.fetch;
     window.fetch = function() {
         var args = arguments;
-        var url = args[0];
-        if (typeof url === 'object') url = url.url;
+        var url = (typeof args[0] === 'string') ? args[0] : (args[0] && args[0].url);
 
-        // Surgical Check: Only touch the ad-logic JSON
-        if (typeof url === 'string' && url.indexOf('spclient') > -1 && url.indexOf('ad-logic') > -1) {
+        // 2. ULTRA-SPECIFIC FILTER: 
+        // Only intercept if it's 'spclient' AND 'ad-logic' (this avoids audio/scripts)
+        if (url && url.indexOf('spclient') > -1 && url.indexOf('ad-logic') > -1) {
             return _fetch.apply(this, args).then(function(response) {
-                return response.clone().json().then(function(data) {
-                    // Clean the data without breaking the object structure
-                    if (data.adSlots) data.adSlots = [];
-                    if (data.ads) data.ads = [];
-                    return new Response(JSON.stringify(data), {
-                        status: response.status,
-                        headers: response.headers
-                    });
-                }).catch(function() { 
-                    return response; // Not JSON? Return untouched to prevent SyntaxError
-                });
+                // 3. SECURE CHECK: Only parse if the server says it's actually JSON
+                var contentType = response.headers.get('content-type');
+                if (contentType && contentType.indexOf('application/json') > -1) {
+                    return response.clone().json().then(function(data) {
+                        if (data.adSlots) data.adSlots = [];
+                        if (data.ads) data.ads = [];
+                        return new Response(JSON.stringify(data), {
+                            status: response.status,
+                            headers: response.headers
+                        });
+                    }).catch(function() { return response; });
+                }
+                return response;
             });
         }
         return _fetch.apply(this, args);
     };
 
-    // DOM Skipper
-    var checkAd = function() {
+    // 4. DOM SKIPPER (Improved)
+    setInterval(function() {
         var skipBtn = document.querySelector('[data-testid="control-button-skip-forward"]');
-        var adLabel = document.querySelector('[data-testid="ad-label"], .encore-advertising-set');
-        if (adLabel && skipBtn && !skipBtn.disabled) {
+        var isAd = !!document.querySelector('[data-testid="ad-label"], .encore-advertising-set');
+        if (isAd && skipBtn && !skipBtn.disabled) {
             skipBtn.click();
+            console.log('[SPOTIFY-AD-SKIP] Ad Detected -> Skip Clicked');
         }
-    };
-
-    setInterval(checkAd, 1000);
+    }, 500);
 })();
